5dd06b539aa3fa9725ad4d14ecc1d932c07721bc
mhsnzkn <muhsinozkan12@gmail.com>
2020-12-08 21:17:19 +0200

    ServerSide PDF download entegrated

--- Html2Pdf/Controllers/HomeController.cs
+++ Html2Pdf/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using Html2Pdf.Models;
+using iText.Html2pdf;
+using iText.Kernel.Pdf;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +24,23 @@ namespace Html2Pdf.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string html)
+        {
+            var workStream = new MemoryStream();
+
+            using (var pdfWriter = new PdfWriter(workStream))
+            {
+                pdfWriter.SetCloseStream(false);
+                using (var document = HtmlConverter.ConvertToDocument(html, pdfWriter))
+                {
+                }
+            }
+            Response.Headers.Add("Content-Disposition", "attachment");
+            Response.Headers.Add("filename", "ServerSide.pdf");
+            workStream.Position = 0;
+            return new FileStreamResult(workStream, "application/pdf");
         }
 
         public IActionResult Privacy()
